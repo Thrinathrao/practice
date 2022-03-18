42dccbd480fda4bf669f8c6a9060262f79cd1b59
thrinath.rao <k.thrinathrao@gmail.com>
2022-03-19 02:48:59 +0530

    changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-       System.out.println("thrinath");
+       System.out.println("thrinath rao");
     }
 }
